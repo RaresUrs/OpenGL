cc52c50e75b78fafa66eed4881c3c1eeedff4bb0
Rares Urs <urs_rares96@yahoo.com>
2016-02-17 22:07:54 +0000

    Update

--- ConsoleApplication1/display.cpp
+++ ConsoleApplication1/display.cpp
@@ -15,6 +15,7 @@ Display::Display(int width, int height, const std::string& title) {
 	//Usually the sum of all the attributes of a pixel
 	SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE, 32);
 
+
 	//Now for the use of the second display
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 	m_window = SDL_CreateWindow(title.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_OPENGL);
